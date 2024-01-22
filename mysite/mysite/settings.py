4272b0627fb6fc10dd38a6eaab5163f6c9901c0b
Mohamed Hanou <66230414+moha-hn@users.noreply.github.com>
2024-01-22 11:24:46 -0500

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -139,6 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_HOST_USER = 
-EMAIL_HOST_PASSWORD = 
+EMAIL_HOST_USER = 'placementmarr@outlook.fr'
+EMAIL_HOST_PASSWORD = "Mouloudia1921"
 
